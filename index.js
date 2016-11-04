c09bc020fd9a23ebd4ae56a13a2596768818043b
Amanda Steckler <amandajsteckler@gmail.com>
2016-11-04 13:44:45 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function getFirstSelector(selector) {
+  return document.querySelector(`${selector}`)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div')
+}
+
+function increaseRankBy(n) {
+  var rankedLists = document.querySelectorAll('.ranked-list')
+  for (let i = 0; i < rankedLists.length; i++) {
+    rankedLists[i].innerHTML = parseInt((rankedLists[i].innerHTML + 1), 10)
+  }
+}
